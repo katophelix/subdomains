2f5d2eda7a23e2087ee4e9da678800e51d2a51bc
Kat Harris <katharrismail@gmail.com>
2019-05-13 15:21:38 -0400

    cruise

--- cal.saltydog.com/__svpro-UxYW0Kw/exec.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	define("SV_KEY", "UxYW0KwVfppMeUP7jiv5nhZkFmT8zzmtu3c1yBljxa5GBwuq7LmOwUaa5eNVKm8KThjqea18vVQqtHxbvupSAHuv0E3DecJxGkWeBzawNNh6tuNy6ChsbWCt2JjmEeMZ");
-
-	$SV_EXTRA_KEYS = array(
-		);
-	require_once("ftp.php");
-	Sv_Handle_Request();
-?>
